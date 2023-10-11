3f4ba922de7141cfc5d91468347b4363e648ef9d
Ramón Miklus <ramon.miklus@gmail.com>
2023-10-12 00:21:55 +0200

    added sequential chain, agent routes

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ from langchain.embeddings import OpenAIEmbeddings
 from routes.llm_chain import llm_chain_route
 from routes.memories import memories_route
 from routes.simple_sequential_chain import simple_sequential_chain_route
+from routes.sequential_chain import sequential_chain_route
+from routes.agent import agent_route
+from routes.agent import python_agent_route
+from routes.agent import custom_agent_route
 
 load_dotenv()
 openai_api_key = os.getenv('OPENAI_API_KEY')
@@ -37,6 +41,10 @@ app = Flask(__name__)
 app.register_blueprint(llm_chain_route)
 app.register_blueprint(memories_route)
 app.register_blueprint(simple_sequential_chain_route)
+app.register_blueprint(sequential_chain_route)
+app.register_blueprint(agent_route)
+app.register_blueprint(python_agent_route)
+app.register_blueprint(custom_agent_route)
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -44,62 +52,6 @@ if __name__ == "__main__":
 @app.route("/")
 def hello_world():
     return 'hello world'
-
-# multiple I/O chain
-@app.route("/sequential_chain")
-def sequential_chain():
-    llm = ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo", openai_api_key=openai_api_key)
-
-    # prompt template 1: translate to english
-    first_prompt = ChatPromptTemplate.from_template(
-        "Translate the following review to english:"
-        "\n\n{Review}"
-    )
-    # chain 1: input= Review and output= English_Review
-    chain_one = LLMChain(llm=llm, prompt=first_prompt, 
-                        output_key="English_Review"
-                        )
-    
-    second_prompt = ChatPromptTemplate.from_template(
-        "Can you summarize the following review in 1 sentence:"
-        "\n\n{English_Review}"
-    )
-    # chain 2: input= English_Review and output= summary
-    chain_two = LLMChain(llm=llm, prompt=second_prompt, 
-                        output_key="summary"
-                        )
-
-    # prompt template 3: translate to english
-    third_prompt = ChatPromptTemplate.from_template(
-        "What language is the following review:\n\n{Review}"
-    )
-    # chain 3: input= Review and output= language
-    chain_three = LLMChain(llm=llm, prompt=third_prompt,
-                        output_key="language"
-                        )
-
-    # prompt template 4: follow up message
-    fourth_prompt = ChatPromptTemplate.from_template(
-        "Write a follow up response to the following "
-        "summary in the specified language:"
-        "\n\nSummary: {summary}\n\nLanguage: {language}"
-    )
-    # chain 4: input= summary, language and output= followup_message
-    chain_four = LLMChain(llm=llm, prompt=fourth_prompt,
-                        output_key="followup_message"
-                        )
-
-    # overall_chain: input= Review 
-    # and output= English_Review,summary, followup_message
-    overall_chain = SequentialChain(
-        chains=[chain_one, chain_two, chain_three, chain_four],
-        input_variables=["Review"],
-        output_variables=["English_Review", "summary","followup_message"],
-        verbose=True)
-    
-    review = "Ottimo prodotto. Impermeabile, comodo, davvero un fantastico paio di scarpe"
-    
-    return overall_chain(review)
 
 @app.route("/router_chain")
 def router_chain():
---
+++ routes/agent.py
@@ -0,0 +1,130 @@
+from flask import Blueprint, request
+from dotenv import load_dotenv
+import os
+
+import langchain
+from langchain.agents.agent_toolkits import create_python_agent
+from langchain.agents import load_tools, initialize_agent
+from langchain.agents import AgentType
+from langchain.tools.python.tool import PythonREPLTool
+from langchain.chat_models import ChatOpenAI
+
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY')
+serpapi_api_key = os.getenv('SERPAPI_API_KEY')
+
+agent_route = Blueprint('agent_route', __name__)
+
+@agent_route.route("/agent")
+def agent():  
+    query = request.args.get("query")
+    print("QUERY", query)
+    llm = ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo", openai_api_key=openai_api_key)
+    # tools = load_tools(["llm-math","wikipedia"], llm=llm)
+    tools = load_tools(["llm-math","serpapi"], llm=llm)
+
+    agent= initialize_agent(
+        tools, 
+        llm, 
+        agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+        handle_parsing_errors=True,
+        verbose = True)
+    
+    return agent(query)
+
+python_agent_route = Blueprint('python_agent_route', __name__)
+
+@python_agent_route.route("/python_agent")
+def python_agent():  
+    llm = ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo", openai_api_key=openai_api_key)
+
+    agent = create_python_agent(
+        llm,
+        tool=PythonREPLTool(),
+        verbose=True,
+        handle_parsing_errors=True
+    )
+
+    customer_list = [
+                ["Harrison", "Chase"], 
+                 ["Lang", "Chain"],
+                 ["Dolly", "Too"],
+                 ["Elle", "Elem"], 
+                 ["Geoff","Fusion"], 
+                 ["Trance","Former"],
+                 ["Jen","Ayai"]
+                ]
+    
+    langchain.debug=True
+    result = agent.run(f"""Sort these customers by \
+        last name and then first name \
+        and print the output: {customer_list}""") 
+    langchain.debug=False
+
+    return result
+
+agent_route = Blueprint('agent_route', __name__)
+
+@agent_route.route("/agent")
+def agent():  
+    query = request.args.get("query")
+    print("QUERY", query)
+    llm = ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo", openai_api_key=openai_api_key)
+    # tools = load_tools(["llm-math","wikipedia"], llm=llm)
+    tools = load_tools(["llm-math","serpapi"], llm=llm)
+
+    agent= initialize_agent(
+        tools, 
+        llm, 
+        agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+        handle_parsing_errors=True,
+        verbose = True)
+    
+    return agent(query)
+
+# tool decorator, it can be applied to any function to transform it in a tool
+from langchain.agents import tool
+from langchain.utilities import TextRequestsWrapper
+from datetime import date
+
+custom_agent_route = Blueprint('custom_agent_route', __name__)
+
+@custom_agent_route.route("/custom_agent")
+def custom_agent():  
+    requests = TextRequestsWrapper()
+
+    # @tool
+    # def time(text: str) -> str:
+    #     f"""Returns todays date, use this for any \
+    #     questions related to knowing todays date. \
+    #     The input should always be an empty string, \
+    #     and this function will always return todays \
+    #     date - any date mathmatics should occur \
+    #     outside this function.{query}"""
+    #     return str(date.today())
+    
+    @tool
+    def api_request(text: str) -> str:
+        """Return the value returned by the function below. It is always a string."""
+        return requests.get("https://webhook.site/54511a40-b58a-49f9-81cd-94cb5780d78a")
+
+    llm = ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo", openai_api_key=openai_api_key)
+    tools = load_tools(["llm-math"], llm=llm)
+
+    agent= initialize_agent(
+        tools + [api_request], 
+        llm, 
+        agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+        handle_parsing_errors=True,
+        verbose = True)
+    
+    langchain.debug=True
+    
+    try:
+        result = agent("Make an API request") 
+    except: 
+        print("exception on external access")
+  
+    langchain.debug=False
+
+    return result
